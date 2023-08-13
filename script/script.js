c86e4a240ed8f651deb5062c76f79369a23f27ea
kadeksinduarta <ikadeksinduarta@gmail.com>
2023-08-13 23:28:54 +0800

    options nya ketika ngeklick ulang quiz engak kelihatan

--- script/script.js
+++ script/script.js
@@ -1,94 +1,125 @@
 const questions = [
-  {
-    image: "img/jokowi.png",
-    question: "Siapa nama tokoh di atas?",
-    answer: "jokowidodo",
-  },
+  // {
+  //   image: "img/jokowi.png",
+  //   question: "Siapa nama tokoh di atas?",
+  //   answer: "jokowidodo",
+  //   options: ["Jokowi", "Prabowo", "Megawati", "Susilo Bambang Yudhoyono"],
+  // },
   {
     // gambar bj habibie
     image: "img/habibie.png",
     question: "Tokoh pada gambar Di atas menjabat sebagai Presiden Ke?",
-    answer: "tiga",
+    answer: "c",
+    options: ["A. satu", "B. dua", "C. tiga", "D. empat"],
   },
   {
     // gambar bintang
     image: "img/bintangSimbol.png",
     question: "gambar di atas merupakan lambang pancasila ke?",
-    answer: "satu",
+    answer: "b",
+    options: ["A. dua", "B. satu", "C. lima", "D. empat"],
   },
   {
     //gambar tuanku iman bonjol
     image: "img/imanBonjol.png",
     question: "berasal dari mana tokoh pada gambar di atas?",
-    answer: "Sumatra Barat",
+    answer: "d",
+    options: ["A. Jawa", "B. Bali", "C. Sumatra Timur", "D. Sumatra Barat"],
   },
   {
     // gambar suekarno
     image: "img/soekarno.png",
     question: "siapa nama tokoh di atas?",
-    answer: "ir.soekarno",
+    answer: "a",
+    options: ["A. Ir. Soekarno", "B. H. Agus Salim", "C. Mr. Moh Yamin", "D. Drs. Moh. Hatta"],
   },
   {
     // gambar pulau jawa
     image: "img/jawa.png",
     question: "gambar peta di atas merupakan peta dari provinsi?",
-    answer: "jawa",
+    answer: "b",
+    options: ["A. Bali", "B. Jawa", "C. Sumatra Timur", "D. Sumatra Barat"],
   },
   {
     // gambar pulau bali
     image: "img/bali.png",
     question: "gambar peta di atas merupakan peta dari provinsi?",
-    answer: "bali",
+    answer: "b",
+    options: ["A. Jawa", "B. Bali", "C. Sumatra Timur", "D. Sumatra Barat"],
   },
   {
     // gambar alat musik ketipung
     image: "img/ketipungJawaT.png",
     question: "alat musik pada gambar di samping berasal dari?",
-    answer: "jawa timur",
+    answer: "c",
+    options: ["A. Sumatra", "B. Bali", "C. jawa Timur", "D. Sumatra Barat"],
   },
   {
     // Gambar : pohon beringin
     image: "img/pohonberingin.png",
     question: "gambar diatas merupakan lambang pancasila ke?",
-    answer: "tiga",
+    answer: "c",
+    options: ["A. satu", "B. dua", "C. tiga", "D. empat"],
   },
   {
     // Gambar : Rantai Emas
     image: "img/rantai.png",
     question: "gambar diatas merupakan lambang pancasila ke?",
-    answer: "dua",
+    answer: "b",
+    options: ["A. satu", "B. lima", "C. tiga", "D. empat"],
   },
   {
     // Gambar : Kepala Banteng
     image: "img/kepalabanteng.png",
     question: "gambar diatas merupakan lambang pancasila ke?",
-    answer: "empat",
+    answer: "c",
+    options: ["A. satu", "B. dua", "C. tiga", "D. lima"],
   },
   {
     // Gambar : Padi dan Kapas.
     image: "img/padikapas.png",
     question: "gambar diatas merupakan lambang pancasila ke?",
-    answer: "lima",
+    answer: "b",
+    options: ["A. satu", "B. lima", "C. tiga", "D. empat"],
   },
 ];
 
 let currentQuestion = 0;
 let correctAnswers = 0;
 let wrongAnswers = 0;
-let totalQuestions = 2;
+let totalQuestions = 5;
 
 const quizImage = document.querySelector(".quiz-container img");
 const quizText = document.querySelector("#quiz");
 const answerInput = document.querySelector("#answer");
 const nextButton = document.querySelector("#next-quiz");
 const restartButton = document.getElementById("restart-quiz");
+const optionsList = document.getElementById("optionLi");
+
+shuffleQuestions();
+displayQuestion(currentQuestion);
 
 function shuffleQuestions() {
   shuffleArray(questions);
 }
 
+questions[currentQuestion].options.forEach((option) => {
+  const liElement = document.createElement("li");
+  liElement.textContent = option;
+  optionsList.appendChild(liElement);
+});
+
 shuffleQuestions(); // Mengacak pertanyaan
 displayQuestion(currentQuestion);
+
+answerInput.addEventListener("input", (event) => {
+  const userInput = event.target.value.toLowerCase();
+
+  // Hanya izinkan huruf a, b, c, atau d
+  if (!["a", "b", "c", "d"].includes(userInput)) {
+    event.target.value = "";
+  }
+});
 
 nextButton.addEventListener("click", () => {
   if (answerInput.value.trim() === "") {
@@ -104,18 +135,35 @@ function restartQuiz() {
   correctAnswers = 0;
   wrongAnswers = 0;
   currentQuestion = 0;
-  shuffleQuestions(); // Mengacak pertanyaan
+  shuffleQuestions();
   displayQuestion(currentQuestion);
+
+  // Hapus elemen pesan benar dan pesan salah
+  document.getElementById("benar").innerHTML = "";
+  document.getElementById("salah").innerHTML = "";
 
   const notification = document.querySelector(".quiz-container p");
   if (notification) {
     notification.remove();
   }
 
+  optionsList.style.display = "block";
   restartButton.style.display = "none";
   answerInput.style.display = "block";
   nextButton.style.display = "block";
   quizImage.style.display = "block";
+
+  // Hapus elemen opsi sebelum mengisi ulang opsi-opsi pertanyaan
+  optionsList.innerHTML = "";
+
+  // Isi ulang opsi-opsi pertanyaan
+  questions[currentQuestion].options.forEach((option) => {
+    const liElement = document.createElement("li");
+    liElement.textContent = option;
+    optionsList.appendChild(liElement);
+  });
+
+  answerInput.value = ""; // Bersihkan nilai input
 }
 
 function shuffleArray(array) {
@@ -129,6 +177,14 @@ function shuffleArray(array) {
 function displayQuestion(questionIndex) {
   quizImage.src = questions[questionIndex].image;
   quizText.textContent = questions[questionIndex].question;
+
+  optionsList.innerHTML = ""; // Clearing previous options
+
+  questions[questionIndex].options.forEach((option) => {
+    const liElement = document.createElement("li");
+    liElement.textContent = option;
+    optionsList.appendChild(liElement);
+  });
 }
 
 function checkAnswer() {
@@ -174,6 +230,7 @@ function displayResult() {
   quizImage.style.display = "none";
   answerInput.style.display = "none";
   nextButton.style.display = "none";
+  optionsList.style.display = "none";
 
   restartButton.addEventListener("click", () => {
     restartQuiz();
@@ -198,7 +255,7 @@ function restartQuiz() {
   correctAnswers = 0;
   wrongAnswers = 0;
   currentQuestion = 0;
-  shuffleArray(questions);
+  shuffleQuestions();
   displayQuestion(currentQuestion);
 
   const notification = document.querySelector(".quiz-container p");
